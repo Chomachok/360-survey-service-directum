5d862f166cebffa61ab489f0f54c94b4dd5f48a3
Evgeniy Dmitriev <evgeniydmit13@gmail.com>
2026-07-09 12:15:04 +0400

    feat: add survey controller

--- backend/Survey360.Api/Controllers/SurveysController.cs
+++ backend/Survey360.Api/Controllers/SurveysController.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Survey360.Api.DTOs.Surveys;
 
 namespace Survey360.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class SurveysController : ControllerBase
+public class SurveysController(ISurveysService surveysService) : ControllerBase
 {
-    
+    [HttpPost]
+    public async Task<ActionResult<SurveyCreateRequest>> Create([FromBody] SurveyCreateRequest surveyCreateRequest)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var createdSurvey = await surveysService.CreateSurveyAsync(surveyCreateRequest);
+        var localUri = $"/api/surveys/{createdSurvey.Id}";
+
+        return Created(localUri, createdSurvey);
+    }
 }
