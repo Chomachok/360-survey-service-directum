68e12c4c467d5cd9492a98944846f591597c9ad4
hanikru <ilya_bazhenov_95@mail.ru>
2026-07-09 13:01:00 +0400

    package.json new

--- frontend/src/create-survey.jsx
+++ frontend/src/create-survey.jsx
@@ -1,1 +1,23 @@
-1
+import {createRoot} from "react-dom/client";
+import {useState} from "react";
+
+
+function createSurvey(){
+    const [name, setName] = useState("Tom");
+
+    function handleNameChange(event) {    
+        setName(event.target.value);  
+    }
+
+    return (
+        <div>
+            <h3>Имя: {name}</h3>
+            <div>
+                <p>Имя: <input type="text" value={name} onChange={handleNameChange} /></p>
+            </div>
+        </div>
+    );
+
+}
+
+createRoot(document.getElementById("name")).render(<createSurvey/>)
