6f51a1e226f762151e8b4dca9182940533d67c46
hanikru <ilya_bazhenov_95@mail.ru>
2026-07-09 14:25:37 +0400

    add StartDate, EndDateChange, TemplateId

--- frontend/src/create-survey.jsx
+++ frontend/src/create-survey.jsx
@@ -2,8 +2,12 @@ import {createRoot} from "react-dom/client";
 import {useState} from "react";
 
 function СreateSurvey(){
-    const [name, setName] = useState("");
+    const [title, setName] = useState("");
     const [description, setDescription] = useState("");
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    const [templateId, setTemplateId] = useState("");
+    const [customQuestions, setTemplateId] = useState("");
 
     function handleNameChange(event) {    
         setName(event.target.value);  
@@ -13,23 +17,50 @@ function СreateSurvey(){
         setDescription(event.target.value);  
     }
 
-    // обработчик отправки формы
+    function handleStartDateChange(event) {    
+        setStartDate(event.target.value);  
+    }
+
+    function handleEndDateChange(event) {    
+        setEndDate(event.target.value);  
+    }
+
+    function handleTemplateId(event) {    
+        setTemplateId(event.target.value);  
+    }
+
     function handleSubmit(e) {
-         e.preventDefault(); // блокируем стандартную отправку формы
-        console.log("Name: ", name);
+         e.preventDefault();
+        console.log("Name: ", title);
         console.log("Description: ", description);
+        console.log("StartDate: ", startDate);
+        console.log("EndDate: ", endDate);
+        console.log("TemplateId: ", templateId);
     }
     
     return (
         <form onSubmit={handleSubmit}>
             <p>
                 <label>Имя:</label><br />
-                <input type="text" value={name} onChange={handleNameChange} />
+                <input type="text" value={title} onChange={handleNameChange} />
             </p>
             <p>
                 <label>Описание:</label><br />
                 <input type="text" value={description} onChange={handleDescriptionChange} />
             </p>
+            <p>
+                <label>Начало опроса:</label><br />   
+                <input type="date" value={startDate} onChange={handleStartDateChange} />
+            </p>
+            <p>
+                <label>Конец опроса:</label><br />   
+                <input type="date" value={endDate} onChange={handleEndDateChange} />
+            </p>
+            <p>
+                <label>ID шаблона:</label><br />   
+                <input type="number" value={templateId} onChange={handleTemplateId} />
+            </p>
+            
             <input type="submit" value="Отправить" />
         </form>
     );
