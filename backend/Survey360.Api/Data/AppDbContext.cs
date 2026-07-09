260616ade2ab8608865e6973d2a72043ed24abb0
sofiiika <shelpyakova05@bk.ru>
2026-07-09 10:38:09 +0400

    feat: add AppDbContext

--- backend/Survey360.Api/Data/AppDbContext.cs
+++ backend/Survey360.Api/Data/AppDbContext.cs
@@ -1,1 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Survey360.Api.Models;
+
 namespace Survey360.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Template> Templates => Set<Template>();
+    public DbSet<TemplateQuestion> TemplateQuestions => Set<TemplateQuestion>();
+    public DbSet<Survey> Surveys => Set<Survey>();
+    public DbSet<Assignment> Assignments => Set<Assignment>();
+    public DbSet<Answer> Answers => Set<Answer>();
+}
