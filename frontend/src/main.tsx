f23b2983c956a07452487fdd94ab13c4379b0e87
Dolbojob <hedoist_1@outlook.com>
2026-07-16 12:27:03 +0400

    merdge fiz

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import SurveyMatrix from "./MatrixGrid"
 import './index.css'
 import { ThemeProvider } from './contexts/ThemeContext'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -13,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <App />/*App*/
+        <App />
         <Toaster
           position="top-right"
           toastOptions={{
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5223,
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5223',
+        target: 'https://localhost:5224',
         changeOrigin: true,
         secure: false,
       },
